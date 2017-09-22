e117ae39d4bdd7d69cfb9688b73121f790d42f5b
Carlota Bindner <cev.bindner@gmail.com>
2017-09-22 19:33:27 +0000

    automatically backed up by learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,7 +2,6 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  extend MetaDancing
   include Dance
 
   attr_accessor :name
@@ -12,3 +11,4 @@ class Dancer
   end
 
 end
+s
